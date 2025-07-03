9ee4b31333a6c8ef2b7c53c1c01ec7ad3cc8bad8
Oleh Levchenko <leva13007@gmail.com>
2025-07-03 21:40:30 +0100

    feat: added Game classes

---
+++ src/main.ts
@@ -0,0 +1,148 @@
+import './style.css';
+import { clamp } from "gamekit-utils";
+
+type MoveVector = -1 | 0 | 1;
+
+const config = {
+  CELL_COUNT: 13,
+  CELL_SIZE: 30,
+  SPRITE_TANK_SIZE: 16,
+  get GRID_SIZE() {
+    return this.CELL_COUNT * this.CELL_SIZE
+  }
+}
+
+class Tank {
+  private isMovingTank = false;
+  private tankVelosity = .12; //px per Msecond
+  private x = 0;
+  private y = 0;
+  private vectorMove: [MoveVector, MoveVector] = [0, 1]; // [dX, dY]
+  private tank1DirrectionMap: Record<string, number> = {
+    "0,-1": 0, // up
+    "-1,0": 2, // left
+    "0,1": 4, // down
+    "1,0": 6, // right
+  }
+
+  constructor() { }
+
+  draw(ctx: CanvasRenderingContext2D, img: HTMLImageElement) {
+    const x = this.getSpriteOffetX();
+    ctx!.drawImage(
+      img,
+      x, 0, config.SPRITE_TANK_SIZE, config.SPRITE_TANK_SIZE, // add tank level 2sd * level
+      this.x, this.y, config.CELL_SIZE, config.CELL_SIZE
+    );
+  }
+
+  getSpriteOffetX() {
+    return config.SPRITE_TANK_SIZE * this.tank1DirrectionMap[`${this.vectorMove[0]},${this.vectorMove[1]}`];
+  }
+
+  move(deltaTime: number) {
+    if (!this.isMovingTank) return;
+    const distance = this.tankVelosity * deltaTime;
+
+    this.x += distance * this.vectorMove[0];
+    this.y += distance * this.vectorMove[1];
+
+    this.x = clamp(this.x, 0, config.GRID_SIZE - config.CELL_SIZE);
+    this.y = clamp(this.y, 0, config.GRID_SIZE - config.CELL_SIZE);
+  }
+
+  setDirection(vector: [MoveVector, MoveVector]){
+    this.vectorMove = vector;
+  }
+
+  setMoving(moving: boolean) {
+    this.isMovingTank = moving;
+  }
+}
+
+class Game {
+
+  private lastTimeStamp = 0;
+  private ctx!: CanvasRenderingContext2D;
+  private player1: Tank;
+  public spriteImg: HTMLImageElement;
+
+  constructor() {
+    this.spriteImg = new Image();
+    this.loadAssets();
+    this.player1 = new Tank();
+    this.canvasSetup();
+    this.setupEventListeners();
+  }
+
+  loadAssets() {
+    this.spriteImg.src = "image.png";
+    this.spriteImg.onload = () => {
+      this.animate(performance.now());
+    };
+    this.spriteImg.onerror = () => {
+      console.error('Failed to load image');
+    };
+  }
+
+  canvasGridSize() {
+    return config.CELL_COUNT * config.CELL_SIZE;
+  }
+
+  canvasSetup() {
+    document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+      <canvas id="canvas" ></canvas>
+    `;
+
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+
+    canvas.width = this.canvasGridSize();
+    canvas.height = this.canvasGridSize();
+
+    this.ctx = canvas!.getContext("2d") as CanvasRenderingContext2D; // TODO fix it
+  }
+
+  private animate(timestamp: number): void {
+    const deltaTime = timestamp - this.lastTimeStamp;
+    this.lastTimeStamp = timestamp;
+
+    this.player1.move(deltaTime)
+    this.render()
+
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  render() {
+    this.ctx!.clearRect(0, 0, config.GRID_SIZE, config.GRID_SIZE);
+    this.player1.draw(this.ctx, this.spriteImg);
+  }
+
+  private setupEventListeners() {
+    window.addEventListener('keydown', this.handleKeyDown.bind(this));
+    window.addEventListener('keyup', this.handleKeyUp.bind(this));
+  }
+
+  private handleKeyDown(e: KeyboardEvent) {
+    switch (e.key) {
+      case 'ArrowRight':
+        this.player1.setDirection([1, 0]);
+        break;
+      case 'ArrowLeft':
+        this.player1.setDirection([-1, 0]);
+        break;
+      case 'ArrowUp':
+        this.player1.setDirection([0, -1]);
+        break;
+      case 'ArrowDown':
+        this.player1.setDirection([0, 1]);
+        break;
+    }
+    this.player1.setMoving(true);
+  }
+  private handleKeyUp() {
+    this.player1.setMoving(false);
+  }
+}
+
+
+new Game();
